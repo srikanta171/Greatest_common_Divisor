736bea8f71e81836815078eeb4eb4b4000578ad5
SRIKANTA BHAINSA <58320116+srikanta171@users.noreply.github.com>
2023-12-15 19:53:12 +0530

    Update cotroller.v

--- cotroller.v
+++ cotroller.v
@@ -6,7 +6,7 @@
 // Create Date: 16.11.2023 13:27:45
 // Design Name: 
 // Module Name: cotroller
-// Project Name: 
+// Project Name: controller designed for GCD data path 
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -33,13 +33,13 @@ output reg a_ld,
 output reg b_ld,
 output reg op_en,
 output reg done,
-output [3:0]state_ctrl
+    output [3:0]state_ctrl  /// port for tracking the change of different states, so the debugging can be easy. 
     );
-reg [3:0]state;
+    reg [3:0]state; /// state register
 parameter s0=0, s1=1, s2=2, s3=3, s4=4, s5=5, s6=6, s7=7, s8=8 ;
 
 
-///this always steatemet for state trasition  accordig to the input at every risig clock at a perticualr state 
+///this always statement for state transition according to the input at every rising clock at a particular state 
 always @(posedge clk or posedge rst)
 begin 
 case (state)
@@ -60,7 +60,7 @@ endcase
 end
 
 
-////when state changes it gives some output accordig to the output  sigal which will cntrol the data path
+////when the state changes it gives some output according to the output  signal which will control the data path
 always @ (state)
 begin
 case (state)
@@ -76,7 +76,7 @@ s8: begin  a_sel=0;b_sel=0;a_ld=0;b_ld=0;op_en = 0;done = 1;end
 default : begin a_sel=0;b_sel=0;a_ld = 0;b_ld = 0;op_en = 0;done = 0;end
 endcase
 end
- assign state_ctrl = state;   
+ assign state_ctrl = state;   // this line is to track the state of the controller.
 endmodule
 
 
